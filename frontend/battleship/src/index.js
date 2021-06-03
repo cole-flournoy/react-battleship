37a9898fe4dd6f4d3dbb6d47b6f5d41386a73b54
Cole Flournoy <flournoy.cole@gmail.com>
2021-06-02 23:46:08 -0400

    Add redux devtools

--- frontend/battleship/src/index.js
+++ frontend/battleship/src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import {createStore} from 'redux'
 import reducer from './reducer'
+import {composeWithDevTools} from 'redux-devtools-extension' 
 
 const initState = {}
-const store = createStore(reducer, initState)
+const store = createStore(reducer, initState, composeWithDevTools())
 
 
 ReactDOM.render(
