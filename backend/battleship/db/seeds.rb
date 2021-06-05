79df5d1d96d1a2498285a64eb3ca81d504f11c85
Cole Flournoy <flournoy.cole@gmail.com>
2021-06-05 00:30:12 -0400

    Add seeds

--- backend/battleship/db/seeds.rb
+++ backend/battleship/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+cole = User.create(username: "cole")
+Game.create([{win: true, accuracy: 50.0, user: cole}, {win: true, accuracy: 75.5, user: cole}, {win: false, accuracy: 32.8, user: cole}, {win: true, accuracy: 99.9, user: cole}])
+
+h = User.create(username: "htrow")
+Game.create([{win: false, accuracy: 43.1, user: h}, {win: true, accuracy: 77.7, user: h}, {win: true, accuracy: 81.2, user: h}, {win: false, accuracy: 56.7, user: h}, {win: false, accuracy: 40.8, user: h}, {win: true, accuracy: 89.4, user: h}])
+
