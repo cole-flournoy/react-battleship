013fa0b8eab5c4e9f4d05b50ba1e2d6576e1a8b0
Cole Flournoy <flournoy.cole@gmail.com>
2021-06-05 02:31:43 -0400

    Fix routing error

--- frontend/battleship/src/App.js
+++ frontend/battleship/src/App.js
@@ -4,8 +4,9 @@ import {Route} from 'react-router-dom'
 function App() {
     return (
     <>
-      <Route path='/play' render={(routerProps) => <Game {...routerProps}/> }/>
-        
+      <Route path='/play'>
+        <Game/>
+      </Route>
       <Route path='/leaderboard'>
 
       </Route>
