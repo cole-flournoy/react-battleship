825c014cdebb312ec955bf6ce2934515a43c87af
Cole Flournoy <flournoy.cole@gmail.com>
2021-06-05 01:02:14 -0400

    Set up controller actions

--- backend/battleship/app/controllers/games_controller.rb
+++ backend/battleship/app/controllers/games_controller.rb
@@ -1,2 +1,18 @@
 class GamesController < ApplicationController
+
+  def create
+    game = Game.new(game_params)
+    if game.save
+      render json: game
+    else 
+      render json: {error: game.errors.full_messages}
+    end
+  end
+
+  private
+  
+  def game_params
+    params.require(:game).permit(:win, :accuracy, :user_id)
+  end
+
 end
--- backend/battleship/app/controllers/users_controller.rb
+++ backend/battleship/app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def index
+    render json: User.all
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else 
+      render json: {error: user.errors.full_messages}
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:username)
+  end
 end
